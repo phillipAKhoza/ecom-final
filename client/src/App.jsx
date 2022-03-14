d8102fe7f5c83a5e59a5e2ff2c35acdf0e1a22f4
Phillip Khoza <phillipantonk@gmail.com>
2022-03-15 01:03:52 +0200

    login rule

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,18 +8,20 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Navigate
 } from "react-router-dom";
 
 const App = () => {
+  const user = true;
   return (
     <Router>
       <Routes>
         <Route path="/" exact element={<Home/>} />
-        <Route path="/products/:category" exact element={<ProductList/>} />
-        <Route path="/product/:id" exact element={<Product/>} />
-        <Route path="/cart" exact element={<Cart/>} />
-        <Route path="/login" exact element={<Login/>} />
-        <Route path="/register" exact element={<Register/>} />
+        <Route path="/products/:category" element={<ProductList/>} />
+        <Route path="/product/:id" element={<Product/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/login" element={user ? <Navigate to="/" /> :<Login/>} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
     </Router>
   );
