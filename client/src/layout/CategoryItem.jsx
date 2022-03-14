722a95b01bb4de395eac762e95c3322d9b428d21
Phillip Khoza <phillipantonk@gmail.com>
2022-03-15 01:14:22 +0200

    categore links

--- client/src/layout/CategoryItem.jsx
+++ client/src/layout/CategoryItem.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { mobile } from "../responsive";
+import {
+    Link,
+  } from "react-router-dom";
 
 const Container = styled.div`
  flex: 1;
@@ -41,11 +44,13 @@ ${mobile({padding: "15px"})};
 const CategoryItem = ({category}) => {
     return (
         <Container>
+            <Link to={`/products/${category.cat}`}>
             <Image src={category.img}/>
             <Info>
                 <Title>{category.title}</Title>
                 <Button>SHOP NOW</Button>
             </Info>
+            </Link>
         </Container>
     )
 }
