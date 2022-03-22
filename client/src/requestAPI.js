ee423dbb668b80a96a7837303b039859d46ed6ad
Phillip Khoza <phillipantonk@gmail.com>
2022-03-22 23:58:49 +0200

    acess token

--- client/src/requestAPI.js
+++ client/src/requestAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOGI2Y2Y4MzBlMWU4YWNkNjYzMzFmYyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NzU0MjkxMSwiZXhwIjoxNjQ3ODAyMTExfQ.z5HKj0jGyRDlJfhx0pb256Nr079G4XgWw1BB-BrSunA";
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOGI2Y2Y4MzBlMWU4YWNkNjYzMzFmYyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0Nzk4NjI5NiwiZXhwIjoxNjQ4MjQ1NDk2fQ.x43Uk-Kn1D5DSur_VoLde6tfFPBXFmn6tJS-WEO2KZM";
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
