f667a017f90447488759d6faa7e3b406cd927bf7
Phillip Khoza <phillipantonk@gmail.com>
2022-03-24 11:53:04 +0200

    use of corse

--- server/index.js
+++ server/index.js
@@ -7,8 +7,10 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const cors = require('cors');
 
 dotenv.config();
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=> console.log("DB Connection Successful!"))
